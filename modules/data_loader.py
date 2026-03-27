46e2ad06eea2cfecc75be58b5acf4f0d48e4d4a5
Rawisara Chairat <rawisara.chat@gmail.com>
2026-03-27 05:08:03 -0400

    change some function and add database

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,23 +1,29 @@
 import pandas as pd
+import sqlite3
 
 # Function to read the csv file
 def load_data(file_path):
     data = pd.read_csv(file_path)
     return data
 
-# Create a schema based on the data types of the columns
-def create_schema(data):
-    schema = {}
+# Function to get the type of each column to create a schema for the database
+def get_column_types(data):
+    types = {}
     for col in data.columns:
         data_type = str(data[col].dtype)
         if 'int' in data_type:
-            schema[col] = 'INTEGER'
+            types[col] = 'INTEGER'
         elif 'float' in data_type:
-            schema[col] = 'REAL'
+            types[col] = 'REAL'
         else:
-            schema[col] = 'TEXT' 
-    return schema
+            types[col] = 'TEXT' 
+    return types
+    
 
-# Insert the data into the database
-def insert_data(data):
-    ...
+# Insert the data into the SQLite database
+def insert_data(data, db="database.db", table_name="table"):
+    # Get column types from the csv data
+    column_types = get_column_types(data)
+    
+    
+    
