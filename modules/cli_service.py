9f0b84ec65385b7d6847a07a76644c9d221ff3ae
Rawisara Chairat <rawisara.chat@gmail.com>
2026-04-04 23:52:28 -0400

    update cli_service.py with schema_manager added

--- modules/cli_service.py
+++ modules/cli_service.py
@@ -3,16 +3,34 @@ import sqlite3
 import sys
 
 from data_loader import load_data, insert_data
+from schema_manager import append_csv_to_table, get_existing_table, create_table, resolve_schema, infer_schema
 
 if __name__ == "__main__":
+    # Perform data_loader 
     if len(sys.argv) != 3:
         print("Usage: python3 cli_service.py \"user question\" \"csv_file_path\"")
         sys.exit(1)
     user_query = sys.argv[1]
     csv_file_path = sys.argv[2]
-    db_file_path = f"data/{csv_file_path.split('/')[-1].split('.')[0]}.db"
+    db_file_path = f"data/music_movie.db"
     data = load_data(csv_file_path)
-    insert_data(data, db_file_path, table_name="spotify_data")
+    table_name = f"{csv_file_path.split('/')[-1].split('.')[0]}_table"
     print(f"Data loaded successfully into {db_file_path}!")
-    print("User question:", user_query)
+    
+    # Perform schema_manager
+    existing_table = get_existing_table(db_file_path, table_name)
+    new_schema = infer_schema(data)
+    action = resolve_schema(db_file_path, table_name, new_schema)
+    if action == "create":
+        create_table(db_file_path, table_name, new_schema)
+        append_csv_to_table(db_file_path, table_name, data)
+        print(f"Table '{table_name}' created successfully.")
+
+    elif action == "append":
+        print("Schema matches. Skipping insert to avoid duplicates.")
+    
+    elif action == "conflict":
+        print(f"Schema conflict detected for table '{table_name}'!")
+
+    print(f"Schema Manager Done!")
     
